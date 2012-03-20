8bec62fe026ae0060f2982f53ec1001f9290ad9d
simar <simar.i3r@gmail.com>
2012-03-20 13:44:26 +0530

    structured exp - rectangle
    Former-commit-id: d20d32cbceec565ce6eb9ede0d55d2c6ec3000e7

---
+++ Matlab/experiments/SVT_MostRecent/a.m
@@ -0,0 +1,11 @@
+Y=[ 5.06 4.60 8.42 ; 0 0 0  ];
+h = bar('v6',Y);
+set(h(1),'facecolor','red') % use color name
+set(h(2),'facecolor',[0 1 0]) % or use RGB triple
+set(h(3),'facecolor','b') % or use a color defined in the help for PLOT
+
+Y=[ 1.27 4.95 ; 1.72 8.83; 5.33 6.43; 6.55 20.21  ];
+h = bar(Y);
+set(h(1),'facecolor','red') % use color name
+set(h(2),'facecolor',[0 1 0]) % or use RGB triple
+set(h(3),'facecolor','b') % or use a color defined in the help for PLOT
